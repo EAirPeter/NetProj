240ab780609a1833b83eaac5de10231966631383
azure_crab <wuwanquan@126.com>
2018-01-06 16:31:27 +0800

    modify compute

--- compute.py
+++ compute.py
@@ -11,7 +11,7 @@ def GetValidNode(content : bytes, height : bytes, id : bytes,
     assert len(content) == 32 and len(height) == 8 and len(id) == 4 \
            and len(timestamp) == 20 and len(parentHash) == 32
     beg = random.randint(10000000000000000000,100000000000000000000)
-    for i in range(beg, beg + 10000000000000000000000):
+    for i in range(beg, beg + 1000000000000000000):
         nounce = b'  ' + bytes(str(i).rjust(20,' '), 'utf-8') + b'  '
         tryBlock = content + height + id + timestamp + nounce + parentHash
         if GetMd5AsHex(tryBlock) < b'0000'.ljust(32, b'f'):
