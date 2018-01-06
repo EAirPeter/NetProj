65b859e4b07776e0cb5047f13fa9856f523d2ab3
EAirPeter <VioletCrestfall@hotmail.com>
2018-01-06 17:05:54 +0900

    sync: minor bug fixed

--- sync.py
+++ sync.py
@@ -27,44 +27,43 @@ class Sync:
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._sock.setblocking(False)
         self._sock.bind(localAddr)
-        self._buf = bytes(kBufLen)
         self._rto = rto
         self._seq = 0
 
-    def DoSync() -> None:
+    def DoSync(self) -> None:
         while True:
-            res = _RecvPending()
+            res = self._RecvPending()
             if res is None:
                 break
-            _Recover(res)
-        _RandomPush()
+            self._Recover(res)
+        self._RandomPush()
 
-    def _RecvPending() -> Optional[bool]:
+    def _RecvPending(self) -> Optional[bool]:
         while True:
-            (peer, pkt) = _RecvPkt(False)
+            (peer, pkt) = self._RecvPkt()
             if peer is None:
                 break
             if pkt[0] == kNmPsh:
-                res = _OnNmPsh(peer, pkt[1], pkt[2], pkt[3])
+                res = self._OnNmPsh(peer, pkt[1], pkt[2], pkt[3])
                 if res is not None:
                     return res
             elif pkt[0] == kRvPll:
-                _OnRvPll(peer, pkt[1], pkt[2], pkt[3])
+                self._OnRvPll(peer, pkt[1], pkt[2], pkt[3])
         return None
     
-    def _RandomPush() -> None:
+    def _RandomPush(self) -> None:
         for peer in random.sample(self._peers, 2):
-            _DoNmPsh(peer)
+            self._DoNmPsh(peer)
 
-    def _Recover(shrink: bool) -> None:
+    def _Recover(self, shrink: bool) -> None:
         if shrink:
-            _Shrink()
+            self._Shrink()
         while True:
             hbeg = self._bc.GetHeight()
-            hend = hbeg + kNBlock
+            hend = hbeg + Sync.kNBlock
             pshs: List[Tuple[int, bytes]] = None
             while not pshs:
-                pshs = _PullRound(hbeg, hend)
+                pshs = self._PullRound(hbeg, hend)
                 pshs = [psh for psh in pshs if psh[0] >= self._bc.GetHeight()]
             hmax = 0
             for psh in pshs:
@@ -83,18 +82,18 @@ class Sync:
                 else:
                     self._bc.Update(pshs[1][1], False)
             if res == 1:
-                _Shrink()
+                self._Shrink()
             if hmax < self._bc.GetHeight() + 6:
                 break
 
-    def _Shrink() -> None:
+    def _Shrink(self) -> None:
         hmax = self._bc.GetHeight()
         res = 1
         while res == 1:
-            hbeg = max(0, hmax - kNBlock)
+            hbeg = max(0, hmax - Sync.kNBlock)
             pshs: List[Tuple[int, bytes]] = None
             while not pshs:
-                pshs = _PullRound(hbeg, hmax)
+                pshs = self._PullRound(hbeg, hmax)
                 pshs = [psh for psh in pshs if psh[0] >= self._bc.GetHeight()]
             if len[pshs] == 1:
                 res = self._bc.Update(pshs[0][1], True)
@@ -109,13 +108,13 @@ class Sync:
                 else:
                     self._bc.Update(pshs[1][1], False)
     
-    def _PullRound(hbeg: int, hend: int) -> List[Tuple[int, bytes]]:
-        seqs = [_DoRvPll(peer, hbeg, hend) for peer in random.sample(self._peers, 2)]
+    def _PullRound(self, hbeg: int, hend: int) -> List[Tuple[int, bytes]]:
+        seqs = [self._DoRvPll(peer, hbeg, hend) for peer in random.sample(self._peers, 2)]
         pshs: List[Tuple[int, bytes]] = list()
         due = time.time() + self._rto
         while True:
             while True:
-                (peer, pkt) = _RecvPkt()
+                (peer, pkt) = self._RecvPkt()
                 if peer is None:
                     break
                 if pkt[0] != kRvPsh:
@@ -130,11 +129,11 @@ class Sync:
                 break
         return pshs
 
-    def _OnNmPsh(peer: SockAddr, seqnum: int, hmax: int, blocks: bytes) -> Optional[bool]:
+    def _OnNmPsh(self, peer: SockAddr, seqnum: int, hmax: int, blocks: bytes) -> Optional[bool]:
         if seqnum != 0:
             return None
         if hmax < self._bc.GetHeight():
-            _DoNmPsh(peer)
+            self._DoNmPsh(peer)
             return None
         res = self._bc.Update(blocks, False)
         if res == 0:
@@ -142,43 +141,46 @@ class Sync:
         elif res == 1:
             return True
         elif res == 2:
-            _DoNmPsh(peer)
+            self._DoNmPsh(peer)
         return None
 
-    def _OnRvPll(peer: SockAddr, seqnum: int, hbeg: int, hend: int) -> None:
+    def _OnRvPll(self, peer: SockAddr, seqnum: int, hbeg: int, hend: int) -> None:
         hmax = self._bc.GetHeight()
         blocks = self._bc.GetRangeRaw(hbeg, hend)
         self._sock.sendto(struct.pack('<sLHs', kRvPsh, seqnum, hmax, blocks))
 
-    def _DoRvPll(peer: SockAddr, hbeg: int, hend: int) -> int:
+    def _DoRvPll(self, peer: SockAddr, hbeg: int, hend: int) -> int:
         seqnum = self._seq
         self._seq += 1
         self._sock.sendto(struct.pack('<sLHH', kRvPll, seqnum, hbeg, hend))
         return seqnum
 
-    def _DoNmPsh(peer: SockAddr) -> None:
+    def _DoNmPsh(self, peer: SockAddr) -> None:
         hmax = self._bc.GetHeight()
-        hbeg = max(0, hmax - kNBlock)
+        hbeg = max(0, hmax - Sync.kNBlock)
         blocks = self._bc.GetRangeRaw(hbeg, hmax)
         self._sock.sendto(struct.pack('<sLHs', kNmPsh, 0, hmax, blocks))
     
-    def _RecvPkt() -> Tuple[Optional[SockAddr], Optional[tuple]]:
+    def _RecvPkt(self) -> Tuple[Optional[SockAddr], Optional[tuple]]:
         while True:
-            (res, peer) = self._sock.recvfrom_into(self._buf, len(self._buf))
-            if res == -1:
+            try:
+                (data, peer) = self._sock.recvfrom(Sync.kBufLen)
+            except BlockingIOError:
+                data = None
+            if data is None:
                 break
-            if res < 8:
+            if len(data) < 8:
                 continue
-            header = self._buf[:4]
+            header = data[:4]
             if header == kNmPsh or header == kRvPsh:
-                if res < 10 or (res - 10) % kZBlock != 0:
+                if len(data) < 10 or (len(data) - 10) % Sync.kZBlock != 0:
                     continue
-                (seqnum, hmax) = struct.unpack('<LH', self._buf[4:])
-                blocks = self._buf[10:res]
+                (seqnum, hmax) = struct.unpack('<LH', data[4:])
+                blocks = data[10:]
                 return (peer, (header, seqnum, hmax, blocks))
             elif header == kRvPll:
-                if res != 12:
+                if len(data) != 12:
                     continue
-                (seqnum, hbeg, hend) = struct.unpack('<LHH', self._buf[4:])
+                (seqnum, hbeg, hend) = struct.unpack('<LHH', data[4:])
                 return (peer, (header, seqnum, hbeg, hend))
         return (None, None)
